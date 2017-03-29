7d9bfd928854e5f0af6283919d645aba927a3785
Junhyuk Park <akdltjswnsgu@naver.com>
2017-03-30 00:22:52 +0900

    정종이 nowPosition맘에안든대서 click으로바꿈

--- src/app/touchscreen/touchscreen.component.ts
+++ src/app/touchscreen/touchscreen.component.ts
@@ -44,14 +44,14 @@ export class TouchscreenComponent implements OnInit {
 
   movePosition() {
     if(!AppComponent.showMoveRange) return;
-    let nowPosition = this.getPosition();
+    let clickPosition = this.getPosition();
     let beforePosition = AppComponent.player.getPosition();
-    if (nowPosition[0] < 1 || nowPosition[0] > AppComponent.gridLineCount || nowPosition[1] < 1 || nowPosition[1] > AppComponent.gridLineCount) {
+    if (clickPosition[0] < 1 || clickPosition[0] > AppComponent.gridLineCount || clickPosition[1] < 1 || clickPosition[1] > AppComponent.gridLineCount) {
       console.log("화면밖");
-    } else if(Math.abs(nowPosition[0]-beforePosition[0]) + Math.abs(nowPosition[1]-beforePosition[1]) > 1) {
+    } else if(Math.abs(clickPosition[0]-beforePosition[0]) + Math.abs(clickPosition[1]-beforePosition[1]) > 1) {
       console.log("플레이어주변아님");
     } else {
-      AppComponent.player.setPosition(nowPosition);
+      AppComponent.player.setPosition(clickPosition);
       AppComponent.showMoveRange = false;
       AppComponent.showAttackRange = true;
     }
@@ -59,7 +59,7 @@ export class TouchscreenComponent implements OnInit {
 
   attackEnemy() {
     if(!AppComponent.showAttackRange) return;
-    let nowPosition = this.getPosition();
+    let clickPosition = this.getPosition();
     let enemyPosition = AppComponent.enemy.getPosition();
 
     let attackarr = new Array<[number, number]>();
@@ -67,8 +67,8 @@ export class TouchscreenComponent implements OnInit {
     attackarr = this.attackrangeComponent.getWeaponRange(AppComponent.player.getWeapon());
     
     attackarr.forEach(element => {
-      if( ( nowPosition[0] == parseInt((element[1]/this.gridWidth+1).toString()) ) && ( nowPosition[1] == parseInt((element[0]/this.gridWidth+1).toString()) ) ) {
-        if(enemyPosition[0]==nowPosition[0] && enemyPosition[1]==nowPosition[1]) {
+      if( ( clickPosition[0] == parseInt((element[1]/this.gridWidth+1).toString()) ) && ( clickPosition[1] == parseInt((element[0]/this.gridWidth+1).toString()) ) ) {
+        if(enemyPosition[0]==clickPosition[0] && enemyPosition[1]==clickPosition[1]) {
           AppComponent.enemy.decrimentHP(1);
           AppComponent.showAttackRange = false;
           //턴넘김 설정
